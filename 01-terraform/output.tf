c2908d9341c3bbff0a391619032699bc4ac7321a
Ekene Ikogbwe <broekeson@gmail.com>
2023-03-19 23:15:37 +0100

    d

--- 01-terraform/output.tf
+++ 01-terraform/output.tf
@@ -6,5 +6,5 @@ output "kubeconfig" {
 resource "local_file" "kubeconfig" {
   content = digitalocean_kubernetes_cluster.altschool_cluster.kube_config[0].raw_config
   filename = "kubeconfig.yaml"
-  file_permission = "400 "
+  file_permission = "400"
 }
