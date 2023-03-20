576c2970e5547afee78de84f7cc1655a7a522837
Ekene Ikogbwe <broekeson@gmail.com>
2023-03-20 14:06:11 +0100

    update

--- 05-terraform/terraform/main.tf
+++ 05-terraform/terraform/main.tf
@@ -8,7 +8,7 @@ data "digitalocean_kubernetes_cluster" "primary" {
 }
 
 data "digitalocean_loadbalancer" "alb" {
-  name = data.digitalocean_kubernetes_cluster.primary.load_balancer_name
+  name = data.digitalocean_kubernetes_cluster.primary.load_balancer.0.name
 }
 
 resource "digitalocean_domain" "mydomain" {
